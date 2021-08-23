9fa3310ff9186c916c772848a60e9358a239d64a
ariel <ariel.ep91@gmail.com>
2021-08-23 01:25:14 -0300

    agrega solución de los ejercicios

--- app/src/test/java/paradigms/clase8/Ejercicios.java
+++ app/src/test/java/paradigms/clase8/Ejercicios.java
@@ -4,7 +4,9 @@ package paradigms.clase8;
 import io.reactivex.rxjava3.core.Observable;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Resuelve cada uno de los ejercicios según el enunciado del test. Imprime cada ejecución de forma que se pueda
@@ -19,6 +21,10 @@ public class Ejercicios {
     @Test
     void multiplicaTodoPor2() {
         final var source = Observable.fromIterable(List.of(1, 2, 3, 4, 5, 6));
+
+        source.map(integer -> integer * 2)
+                .subscribe(System.out::println);
+
     }
 
     /** TODO:
@@ -27,6 +33,10 @@ public class Ejercicios {
     @Test
     void multiplicaLosParesPor2() {
         final var source = Observable.fromIterable(List.of(1, 2, 3, 4, 5, 6));
+
+        source.filter(integer -> integer % 2 != 0)
+                .map(integer -> integer * 2)
+                .subscribe(System.out::println);
 
     }
 
@@ -37,6 +47,9 @@ public class Ejercicios {
     void repite3VecesCadaElemento() {
         final var source = Observable.fromIterable(List.of(1, 2, 3, 4, 5, 6));
 
+        source.flatMap(element -> Observable.fromIterable(Collections.nCopies(3, element)))
+                .subscribe(System.out::println);
+
     }
 
     /** TODO:
@@ -46,6 +59,11 @@ public class Ejercicios {
     void repite3VecesLosParesY1VezLosImpares() {
         final var source = Observable.fromIterable(List.of(1, 2, 3, 4, 5, 6));
 
+        source.flatMap(element -> Optional.of(element % 2 == 0)
+                        .filter(isEven -> isEven)
+                        .map(isEven -> Observable.fromIterable(Collections.nCopies(3, element)))
+                        .orElseGet(() -> Observable.just(element)))
+                .subscribe(System.out::println);
     }
 
     /** TODO:
@@ -54,6 +72,14 @@ public class Ejercicios {
     @Test
     void emiteElementosHastaQueEncuentresUnElementoImpar() {
         final var source = Observable.fromIterable(List.of(1, 2, 3, 4, 5, 6));
+
+        source.doOnNext(element -> {
+                    if (element % 2 == 0) {
+                        throw new RuntimeException("Apaaaa, un valor impa");
+                    }
+                })
+                .subscribe(System.out::println,
+                        Throwable::printStackTrace);
 
     }
 }
--- app/src/test/java/paradigms/clase8/ReactiveStreamsLab.java
+++ app/src/test/java/paradigms/clase8/ReactiveStreamsLab.java
@@ -1,13 +1,15 @@
 package paradigms.clase8;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import io.reactivex.rxjava3.core.Observable;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicInteger;
-import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ReactiveStreamsLab {
 
@@ -16,7 +18,8 @@ public class ReactiveStreamsLab {
     final var singleEvent = "One event... whatever";
     final var observableSource = Observable.just(singleEvent);
 
-    observableSource.subscribe(event -> assertEquals(singleEvent, event));
+    observableSource
+            .subscribe(event -> assertEquals(singleEvent, event));
   }
 
   @Test
@@ -33,14 +36,15 @@ public class ReactiveStreamsLab {
   void observablesLifeCicle() {
     final var multiplesEvents = List.of("a", "b", "c");
     final var observableSource = Observable.fromIterable(multiplesEvents);
-    final var stringBuilder = new StringBuilder();
+
+    final var stringConsumer = new StringBuilder();
 
     observableSource.subscribe(
-        stringBuilder::append, // onNext
+        stringConsumer::append, // onNext
         Throwable::printStackTrace, // onError
-        () -> stringBuilder.append("_sarasa")); // onComplete
+        () -> stringConsumer.append("_sarasa")); // onComplete
 
-    assertEquals("abc_sarasa", stringBuilder.toString());
+    assertEquals("abc_sarasa", stringConsumer.toString());
   }
 
   @Test
@@ -59,30 +63,36 @@ public class ReactiveStreamsLab {
   @Test
   void scanningObservables() {
     final var source = List.of("1", "2", "3", "4");
+
     final var observable = Observable.fromIterable(source);
+
     final var result = new StringBuilder();
     observable.scan("", (a, b) -> a + b).subscribe(result::append);
 
-    assertEquals("1121231234", result.toString());
+    assertEquals("1 12 123 1234", result.toString());
   }
 
   @Test
   void groupingByEvents() {
     final var source = List.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
     final var observable = Observable.fromIterable(source);
+
     int[] PAR = {0};
+
     int[] IMPAR = {0};
 
     observable
-        .groupBy(value -> value % 2 == 0 ? "EVEN" : "ODD")
-        .subscribe(
-            groupedObservable ->
-                groupedObservable.subscribe(
-                    value ->
-                        Optional.of(groupedObservable.getKey().equals("EVEN"))
-                            .filter(isEven -> isEven)
-                            .map(even -> PAR[0] += value)
-                            .orElseGet(() -> IMPAR[0] += value)));
+            .groupBy(value -> value % 2 == 0 ? "PAR" : "IMPAR")
+            .subscribe(
+                    groupedObservable ->
+                            groupedObservable.subscribe(
+                                    value ->
+                                            Optional.of(groupedObservable.getKey().equals("PAR"))
+                                                    .filter(isEven -> isEven)
+                                                    .map(even -> Observable.just(value))
+                                                    .orElseGet(() -> Observable.fromIterable(Collections.nCopies(3, value)))
+                                                    .subscribe(System.out::println)));
 
     assertEquals(30, PAR[0]);
     assertEquals(25, IMPAR[0]);
@@ -91,12 +101,14 @@ public class ReactiveStreamsLab {
   @Test
   void filteringEvents() {
     final var source = List.of(23, 12, 56, 28, 45);
+
     final var observable = Observable.fromIterable(source);
+
     final var accumulator = new AtomicInteger();
 
     observable
-        .filter(value -> value < 30)
-        .subscribe(under30 -> accumulator.getAndAccumulate(under30, Integer::sum));
+        .filter(value -> value < 20)
+        .subscribe(System.out::println);
 
     assertEquals(63, accumulator.get());
   }
